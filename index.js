4c6698c1a7ccc6da417208ffce8b0ca76c2c09d3
Tavnii <Dragoneye1739@gmail.com>
2019-10-27 20:20:00 +0200

    Client creation

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import StorageModule from 'storage-module'
+import TokenModule from "token-module";
+
 import Client from "./src/Clients/Client";
 import Request from "./src/Requests/Request";
 import AxiosClient from "./src/Clients/AxiosClient";
@@ -8,3 +11,23 @@ export default Client;
 export let Request;
 export let AxiosClient;
 export let FetchClient;
+
+export function createClient({ request, tokenModule, server_url, routes, storageModuleOptions, storageKey = 'user', tokenKey = 'access_token' }) {
+  if (!tokenModule) {
+    storageModuleOptions = storageModuleOptions ? storageModuleOptions : {
+      storageList: [localStorage, sessionStorage], defaultStorage: localStorage
+    };
+
+    const storageModule = new StorageModule(storageModuleOptions);
+
+    tokenModule = new TokenModule({
+      storageModule,
+      storageKey,
+      tokenKey
+    })
+  }
+
+  return new Client({
+    request, routes, server_url, tokenModule
+  });
+}
